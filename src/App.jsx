5cd3cc1b0ddb1502590bab183c4c46d426c950aa
karthik-parani <karthikparani2001@gmail.com>
2025-05-27 12:37:38 +0530

    change the first letter upper case to lower case of  sidebar folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./components/Header/Header";
-import Sidebar from "./components/Sidebar/Sidebar";
+import Sidebar from "./components/sidebar/Sidebar";
 import Dashbord from "./components/dashbord/DashbordScreen";
 import Appointments from "./components/appointments/AppointmentsScreen";
 
